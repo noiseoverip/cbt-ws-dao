66694f124c82020cf96734ed9cd8c2bbb33fcfc1
esauali <saulius.alisauskas@ericsson.com>
2013-03-03 21:15:24 +0100

    add userId field to TestProfileDao

--- src/main/java/com/cbt/ws/dao/TestProfileDao.java
+++ src/main/java/com/cbt/ws/dao/TestProfileDao.java
@@ -19,7 +19,7 @@ import com.cbt.ws.mysql.Db;
  * Test profile DAO
  * 
  * @author SauliusALisauskas 2013-03-03 Initial version
- *
+ * 
  */
 public class TestProfileDao {
 
@@ -37,7 +37,8 @@ public class TestProfileDao {
 		for (Record r : result) {
 			TestProfile tc = new TestProfile();
 			tc.setId(r.getValue(TESTPROFILE.TESTPROFILE_ID));
-			tc.setMode(r.getValue(TESTPROFILE.MODE));			
+			tc.setUserId(r.getValue(TESTPROFILE.USER_ID));
+			tc.setMode(r.getValue(TESTPROFILE.MODE));
 			tc.setMetadata(r.getValue(TESTPROFILE.METADATA));
 			TESTPROFILEurations.add(tc);
 			mLogger.debug(tc);
@@ -53,18 +54,19 @@ public class TestProfileDao {
 	 */
 	public Long add(TestProfile testProfile) {
 		Executor sqexec = new Executor(Db.getConnection(), SQLDialect.MYSQL);
-		
+
 		mLogger.trace("Adding new test configuration");
-		Long TESTPROFILEID = sqexec.insertInto(TESTPROFILE, TESTPROFILE.MODE, TESTPROFILE.DEVICETYPECOUNT)
-				.values(testProfile.getMode(), testProfile.getDeviceTypes().size())
+		Long TESTPROFILEID = sqexec
+				.insertInto(TESTPROFILE, TESTPROFILE.USER_ID, TESTPROFILE.MODE, TESTPROFILE.DEVICETYPECOUNT)
+				.values(testProfile.getUserId(), testProfile.getMode(), testProfile.getDeviceTypes().size())
 				.returning(TESTPROFILE.TESTPROFILE_ID).fetchOne().getTestprofileId();
 		mLogger.trace("Added test configuration, enw id:" + TESTPROFILEID);
 		mLogger.trace("Adding device list to test configuration:" + TESTPROFILEID);
-		for (Integer deviceTypeId : testProfile.getDeviceTypes()) { 
+		for (Integer deviceTypeId : testProfile.getDeviceTypes()) {
 			mLogger.trace("Addign device id");
-			//TODO: improve performance here
-			sqexec.insertInto(TESTPROFILE_DEVICES, TESTPROFILE_DEVICES.TESTPROFILE_ID, TESTPROFILE_DEVICES.DEVICE_TYPE_ID)
-					.values(TESTPROFILEID, deviceTypeId).execute();
+			// TODO: improve performance here
+			sqexec.insertInto(TESTPROFILE_DEVICES, TESTPROFILE_DEVICES.TESTPROFILE_ID,
+					TESTPROFILE_DEVICES.DEVICE_TYPE_ID).values(TESTPROFILEID, deviceTypeId).execute();
 		}
 		return TESTPROFILEID;
 	}
