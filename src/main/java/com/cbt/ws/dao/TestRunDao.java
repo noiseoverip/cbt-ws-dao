b2aa15b3cc3e8669a9cced6b0aacd3cfc5924c54
saulius <noiseoverip@gmail.com>
2013-03-10 21:43:32 +0100

    Update TestRunDao
    *add delete
    *add get
    *add update
    *add test

--- src/main/java/com/cbt/ws/dao/TestRunDao.java
+++ src/main/java/com/cbt/ws/dao/TestRunDao.java
@@ -14,13 +14,15 @@ import org.jooq.SQLDialect;
 import org.jooq.impl.Executor;
 
 import com.cbt.ws.entity.TestRun;
+import com.cbt.ws.exceptions.CbtDaoException;
+import com.cbt.ws.jooq.tables.records.TestrunRecord;
 import com.cbt.ws.mysql.Db;
 
 /**
  * Test run DAO
  * 
  * @author SauliusALisauskas 2013-03-03 Initial version
- *
+ * 
  */
 public class TestRunDao {
 
@@ -42,7 +44,7 @@ public class TestRunDao {
 			tr.setCreated(r.getValue(TESTRUN.CREATED));
 			tr.setUpdated(r.getValue(TESTRUN.UPDATED));
 			tr.setStatus(r.getValue(TESTRUN.STATUS));
-			tr.setUserId(r.getValue(TESTRUN.USER_ID));			
+			tr.setUserId(r.getValue(TESTRUN.USER_ID));
 			testRuns.add(tr);
 			mLogger.debug(tr);
 		}
@@ -55,19 +57,58 @@ public class TestRunDao {
 	 * @param userid
 	 * @return
 	 */
-	public Long add(TestRun testConfig) {
+	public Long add(TestRun testRun) {
 		Executor sqexec = new Executor(Db.getConnection(), SQLDialect.MYSQL);
 		mLogger.trace("Starting new test run");
 		Long testRunId = sqexec
-				.insertInto(TESTRUN, 
-						TESTRUN.USER_ID, 
-						TESTRUN.TESTCONFIG_ID, 
-						TESTRUN.CREATED)
-				.values(testConfig.getUserId(), 
-						testConfig.getTestConfigId(), 
-						new Timestamp(Calendar.getInstance().getTimeInMillis()))
-				.returning(TESTRUN.TESTRUN_ID).fetchOne().getTestrunId();
-		mLogger.trace("Added test run, new id:" + testRunId);		
+				.insertInto(TESTRUN, TESTRUN.USER_ID, TESTRUN.TESTCONFIG_ID, TESTRUN.CREATED)
+				.values(testRun.getUserId(), testRun.getTestConfigId(),
+						new Timestamp(Calendar.getInstance().getTimeInMillis())).returning(TESTRUN.TESTRUN_ID)
+				.fetchOne().getTestrunId();
+		mLogger.trace("Added test run, new id:" + testRunId);
 		return testRunId;
+	}
+
+	/**
+	 * Update TestRun record
+	 * 
+	 * @param testRun
+	 * @throws CbtDaoException
+	 */
+	public void update(TestRun testRun) throws CbtDaoException {
+		Executor sqexec = new Executor(Db.getConnection(), SQLDialect.MYSQL);
+		int count = sqexec.update(TESTRUN).set(TESTRUN.STATUS, testRun.getStatus())
+				.where(TESTRUN.TESTRUN_ID.eq(testRun.getId())).execute();
+
+		if (count != 1) {
+			throw new CbtDaoException("Could not update device");
+		}
+	}
+
+	/**
+	 * Get TestRun
+	 * 
+	 * @param testRunId
+	 * @return
+	 */
+	public TestRun getTestRun(Long testRunId) {
+		Executor sqexec = new Executor(Db.getConnection(), SQLDialect.MYSQL);
+		TestrunRecord record = (TestrunRecord) sqexec.select().from(TESTRUN).where(TESTRUN.TESTRUN_ID.eq(testRunId))
+				.fetchOne();
+		return TestRun.fromJooqRecord(record);
+	}
+
+	/**
+	 * Delete testRun
+	 * 
+	 * @param testRun
+	 * @throws CbtDaoException
+	 */
+	public void delete(TestRun testRun) throws CbtDaoException {
+		Executor sqexec = new Executor(Db.getConnection(), SQLDialect.MYSQL);
+		int result = sqexec.delete(TESTRUN).where(TESTRUN.TESTRUN_ID.eq(testRun.getId())).execute();
+		if (result != 1) {
+			throw new CbtDaoException("Error while deleting device, result:" + result);
+		}
 	}
 }
--- src/main/java/com/cbt/ws/entity/TestRun.java
+++ src/main/java/com/cbt/ws/entity/TestRun.java
@@ -1,10 +1,24 @@
 package com.cbt.ws.entity;
 
 import com.cbt.ws.jooq.enums.TestrunStatus;
+import com.cbt.ws.jooq.tables.records.TestrunRecord;
 
 public class TestRun extends CbtEntity {
 	private Long testConfigId;
 	private TestrunStatus status;
+
+	public static TestRun fromJooqRecord(TestrunRecord record) {
+		if (null == record) {
+			return null;
+		}
+		TestRun testRun = new TestRun();
+		testRun.setId(record.getTestrunId());
+		testRun.setStatus(record.getStatus());
+		testRun.setTestConfigId(record.getTestconfigId());
+		testRun.setUpdated(record.getUpdated());
+
+		return testRun;
+	}
 
 	public Long getTestConfigId() {
 		return testConfigId;
@@ -20,6 +34,18 @@ public class TestRun extends CbtEntity {
 
 	public void setStatus(TestrunStatus status) {
 		this.status = status;
+	}
+	
+	@Override
+	public boolean equals(Object object) {
+		if (null != object && object instanceof TestRun) {
+			TestRun other = (TestRun) object;
+			if (getId().equals(other.getId()) && getTestConfigId().equals(other.getTestConfigId())
+					&& getStatus().equals(other.getStatus())) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
