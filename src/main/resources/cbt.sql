4aed86ac1107425c42109d273bf72928d2750f70
saulius <noiseoverip@gmail.com>
2013-03-10 21:48:23 +0100

    Update database schema

--- src/main/resources/cbt.sql
+++ src/main/resources/cbt.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.1
+-- version 3.4.11.1deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 03, 2013 at 09:23 PM
--- Server version: 5.5.24-log
--- PHP Version: 5.3.13
+-- Generation Time: Mar 10, 2013 at 09:47 PM
+-- Server version: 5.5.29
+-- PHP Version: 5.4.6-1ubuntu1.1
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,16 +23,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `devicejobs`
+-- Table structure for table `device`
 --
 
-CREATE TABLE IF NOT EXISTS `devicejobs` (
+CREATE TABLE IF NOT EXISTS `device` (
+  `device_id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `deviceunique_id` char(255) NOT NULL,
+  `devicetype_id` bigint(20) NOT NULL,
+  `state` enum('ONLINE','OFFLINE') NOT NULL DEFAULT 'OFFLINE',
+  `deviceos_id` bigint(50) NOT NULL,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`device_id`),
+  UNIQUE KEY `deviceunique_id` (`deviceunique_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=41 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `device_job`
+--
+
+CREATE TABLE IF NOT EXISTS `device_job` (
   `devicejob_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `device_id` bigint(20) NOT NULL,
   `testrun_id` bigint(20) NOT NULL,
   `created` datetime NOT NULL,
   `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `status` enum('WAITING','RUNNING','FINISHED') NOT NULL DEFAULT 'WAITING',
+  `status` enum('WAITING','CHECKEDOUT','RUNNING','FINISHED') NOT NULL DEFAULT 'WAITING',
   PRIMARY KEY (`devicejob_id`),
   KEY `device_id` (`device_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
@@ -40,28 +58,23 @@ CREATE TABLE IF NOT EXISTS `devicejobs` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `devices`
+-- Table structure for table `device_os`
 --
 
-CREATE TABLE IF NOT EXISTS `devices` (
-  `device_id` bigint(11) NOT NULL AUTO_INCREMENT,
-  `deviceunique_id` char(255) NOT NULL,
-  `devicetype_id` bigint(20) NOT NULL,
-  `state` enum('ONLINE','OFFLINE') NOT NULL,
-  `OS_VERSION` char(50) NOT NULL,
-  `OS_TYPE` char(50) NOT NULL,
-  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`device_id`)
+CREATE TABLE IF NOT EXISTS `device_os` (
+  `deviceos_id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `name` char(255) NOT NULL,
+  PRIMARY KEY (`deviceos_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `devicestypes`
+-- Table structure for table `device_type`
 --
 
-CREATE TABLE IF NOT EXISTS `devicestypes` (
-  `devicetype_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `device_type` (
+  `devicetype_id` bigint(11) NOT NULL AUTO_INCREMENT,
   `model` char(100) NOT NULL,
   `manufacture` char(100) NOT NULL,
   `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -112,7 +125,7 @@ CREATE TABLE IF NOT EXISTS `testpackage` (
   `metadata` longtext,
   `updated` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`testpackage_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
@@ -155,7 +168,7 @@ CREATE TABLE IF NOT EXISTS `testrun` (
   `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `status` enum('WAITING','RUNNING','FINISHED') NOT NULL DEFAULT 'WAITING',
   PRIMARY KEY (`testrun_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 -- --------------------------------------------------------
 
@@ -170,7 +183,7 @@ CREATE TABLE IF NOT EXISTS `testtarget` (
   `updated` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
   `metadata` text,
   PRIMARY KEY (`testtarget_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
