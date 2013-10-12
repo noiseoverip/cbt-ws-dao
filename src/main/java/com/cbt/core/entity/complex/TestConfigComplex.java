bfedccfa8cc290dd2aaa2e6f121eb1b94ea73cb2
sauliusalisauskas <alisauskas.saulius@gmail.com>
2013-10-12 14:27:32 +0200

    extend TestConfigComplex

--- src/main/java/com/cbt/core/entity/complex/TestConfigComplex.java
+++ src/main/java/com/cbt/core/entity/complex/TestConfigComplex.java
@@ -1,53 +1,82 @@
 package com.cbt.core.entity.complex;
 
-import com.cbt.core.entity.CbtEntity;
+import java.util.Date;
+
+import javax.persistence.Column;
+
+import com.cbt.core.entity.TestProfile;
 import com.cbt.core.entity.TestScript;
 import com.cbt.core.entity.TestTarget;
 import com.google.common.base.Objects;
 
 /**
  * Entity representing test configuration data.
- *
+ * 
  * @author SauliusAlisauskas 2013-03-24 Initial version
  */
-public class TestConfigComplex extends CbtEntity {
+public class TestConfigComplex {	
+	private Long id;
+	private String name;
+	private TestProfile testProfile;
+	private TestScript testScript;
+	private TestTarget testTarget;
+	private Date updated;
 
-   private Long testProfileId;
-   private TestScript testScript;
-   private TestTarget testTarget;
+	public Long getId() {
+		return id;
+	}
 
-   public TestScript getTestScript() {
-      return testScript;
-   }
+	public String getName() {
+		return name;
+	}
+	public TestProfile getTestProfile() {
+		return testProfile;
+	}
+	public TestScript getTestScript() {
+		return testScript;
+	}
+	public TestTarget getTestTarget() {
+		return testTarget;
+	}
+	public Date getUpdated() {
+		return updated;
+	}
+	
+	@Column(name = "test_config_id")
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	@Column(name = "test_config_name")
+	public void setName(String name) {
+		this.name = name;
+	}
 
-   public void setTestScript(TestScript testScript) {
-      this.testScript = testScript;
-   }
+	public void setTestProfile(TestProfile testProfile) {
+		this.testProfile = testProfile;
+	}
 
-   public TestTarget getTestTarget() {
-      return testTarget;
-   }
+	public void setTestScript(TestScript testScript) {
+		this.testScript = testScript;
+	}
 
-   public void setTestTarget(TestTarget testTarget) {
-      this.testTarget = testTarget;
-   }
+	public void setTestTarget(TestTarget testTarget) {
+		this.testTarget = testTarget;
+	}	
+	
+	public void setUpdated(Date updated) {
+		this.updated = updated;
+	}
 
-   public Long getTestProfileId() {
-      return testProfileId;
-   }
+	@Override
+	public String toString() {
+		return Objects.toStringHelper(this.getClass())
+				.add("id", getId())
+				.add("testProfile", getTestProfile())
+				.add("testScript", getTestScript())
+				.add("testTarget", getTestTarget())
+				.toString();
 
-   public void setTestProfileId(Long testProfileId) {
-      this.testProfileId = testProfileId;
-   }
-
-   @Override
-   public String toString() {
-      return Objects.toStringHelper(this.getClass())
-            .add("id", getId())
-            .add("userId", getUserId())
-            .add("testScript", getTestScript())
-            .add("testTarget", getTestTarget()).toString();
-
-   }
-
+	}
+	
 }
