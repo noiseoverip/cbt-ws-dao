d874f27b2a128d01facfb81d9870b9f7a8ddfb8d
esauali <saulius.alisauskas@ericsson.com>
2013-10-20 12:12:37 +0200

    improve TestConfig entity

--- src/main/java/com/cbt/core/entity/TestConfig.java
+++ src/main/java/com/cbt/core/entity/TestConfig.java
@@ -1,5 +1,7 @@
 package com.cbt.core.entity;
 
+import javax.persistence.Column;
+
 import com.google.common.base.Objects;
 
 /**
@@ -7,43 +9,13 @@ import com.google.common.base.Objects;
  *
  * @author SauliusAlisauskas 2013-03-24 Initial version
  */
-public class TestConfig extends CbtEntity {
+public class TestConfig {
+   private Long id;
+   private String name;
+   private Long testProfileId;
    private Long testScriptId;
    private Long testTargetId;
-   private Long testProfileId;
-   private Long testConfigId;
-
-   public Long getTestConfigId() {
-      return testConfigId;
-   }
-
-   public void setTestConfigId(Long testConfigId) {
-      this.testConfigId = testConfigId;
-   }
-
-   public Long getTestProfileId() {
-      return testProfileId;
-   }
-
-   public void setTestProfileId(Long testProfileId) {
-      this.testProfileId = testProfileId;
-   }
-
-   public Long getTestScriptId() {
-      return testScriptId;
-   }
-
-   public void setTestScriptId(Long testScriptId) {
-      this.testScriptId = testScriptId;
-   }
-
-   public Long getTestTargetId() {
-      return testTargetId;
-   }
-
-   public void setTestTargetId(Long testTargetId) {
-      this.testTargetId = testTargetId;
-   }
+   private Long userId;
 
    @Override
    public boolean equals(Object obj) {
@@ -57,9 +29,66 @@ public class TestConfig extends CbtEntity {
       return false;
    }
 
+   public Long getId() {
+      return id;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public Long getTestProfileId() {
+      return testProfileId;
+   }
+
+   public Long getTestScriptId() {
+      return testScriptId;
+   }
+
+   public Long getTestTargetId() {
+      return testTargetId;
+   }
+
+   // TODO: set column name to testconfig_user_id
+   public Long getUserId() {
+      return userId;
+   }
+
+   @Column(name = "test_config_id")
+   public void setId(Long id) {
+      this.id = id;
+   }
+
+   @Column(name = "test_config_name")
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   // TODO: should be testconfig_testprofile_id
+   @Column(name = "test_profile_id")
+   public void setTestProfileId(Long testProfileId) {
+      this.testProfileId = testProfileId;
+   }
+
+   // TODO: should be testconfig_testscript_id
+   @Column(name = "test_script_id")
+   public void setTestScriptId(Long testScriptId) {
+      this.testScriptId = testScriptId;
+   }
+
+   // TODO: should be testconfig_testtarget_id
+   @Column(name = "test_target_id")
+   public void setTestTargetId(Long testTargetId) {
+      this.testTargetId = testTargetId;
+   }
+
+   public void setUserId(Long userId) {
+      this.userId = userId;
+   }
+
    @Override
    public String toString() {
-      return Objects.toStringHelper(this.getClass()).add("id", getTestConfigId()).add("testScriptId", testScriptId)
+      return Objects.toStringHelper(this.getClass()).add("id", this.id).add("testScriptId", testScriptId)
             .add("testTargetId", testTargetId).add("testProfileId", testProfileId).toString();
    }
 }
