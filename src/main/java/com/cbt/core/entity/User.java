e0fb2fc2a19b7d042a47889c1d9bc093a628b7ee
esauali <saulius.alisauskas@ericsson.com>
2013-11-05 06:06:29 +0100

    update user entity

--- src/main/java/com/cbt/core/entity/User.java
+++ src/main/java/com/cbt/core/entity/User.java
@@ -1,5 +1,7 @@
 package com.cbt.core.entity;
 
+import javax.persistence.Column;
+
 /**
  * Class for storing user variables
  *
@@ -9,11 +11,22 @@ public class User {
 
    private String name;
    private Long id;
+   private String email;
+
+   public String getEmail() {
+      return email;
+   }
+
+   @Column(name = "user_email")
+   public void setEmail(String email) {
+      this.email = email;
+   }
 
    public Long getId() {
       return id;
    }
 
+   @Column(name = "user_id")
    public void setId(Long id) {
       this.id = id;
    }
@@ -22,6 +35,7 @@ public class User {
       return name;
    }
 
+   @Column(name = "user_name")
    public void setName(String name) {
       this.name = name;
    }
